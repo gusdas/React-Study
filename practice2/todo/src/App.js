f804812b38ac0b2f31f54c1fc65ed6f08557f6a7
SuperHong94 <gustmd213@naver.com>
2022-01-15 01:49:38 +0900

    React Hooks
    useReducer 공부

--- practice2/todo/src/App.js
+++ practice2/todo/src/App.js
@@ -1,8 +1,8 @@
-import { useState, useRef, useCallback } from 'react';
+import { useState, useRef, useCallback, useEffect } from 'react';
 import TodoInsert from './TodoInsert';
 import TodoTemplate from './TodoTemplate';
 import TodoList from './TodoList';
-
+import Test from './Test';
 // import Test from './test';
 function App() {
   const [todos, setTodos] = useState([
@@ -15,14 +15,19 @@ function App() {
     { text: '일정관리 앱 만들어보기', checked: true, id: 3 },
   ]);
   const nextId = useRef(4);
+  const onInsert = useCallback((text) => {
+    const todo = { text, checked: false, id: nextId.current };
+    setTodos(todos.concat(todo));
+    nextId.current += 1;
+  });
 
   return (
     <div>
-      <TodoTemplate>
-        <TodoInsert />
+      {/* <TodoTemplate>
+        <TodoInsert onInsert={onInsert} />
         <TodoList todos={todos} />
-      </TodoTemplate>
-      {/* <Test /> */}
+      </TodoTemplate> */}
+      <Test />
     </div>
   );
 }
--- practice2/todo/src/TodoInsert.js
+++ practice2/todo/src/TodoInsert.js
@@ -1,13 +1,28 @@
 import './TodoInsert.scss';
+import { useState } from 'react';
 import { MdAdd } from 'react-icons/md';
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onInsert(value);
+    setValue('');
+  };
   return (
-    <div className="insertTemplate">
-      <input className="insert" placeholder="할 일을 입력하세요"></input>
-      <div className="button">
+    <form className="insertTemplate" onSubmit={onSubmit}>
+      <input
+        className="insert"
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={(e) => onChange(e)}
+      />
+      <button className="button" type="submit">
         <MdAdd />
-      </div>
-    </div>
+      </button>
+    </form>
   );
 };
 
